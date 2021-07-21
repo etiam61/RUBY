d6aa98c57302e9d2adf88ac8fd46250a58505647
Van Dong <bui.van.dong@sun-asterisk.com>
2021-07-21 09:38:06 +0700

    fix: bai1

--- bai1.rb
+++ bai1.rb
@@ -8,7 +8,7 @@ params = {
       "0"=>{
         "account"=>"847445",
         "branch_name"=>"VCB"
-       
+
       },
       "1"=>{
         "account"=>"985784",
@@ -19,4 +19,31 @@ params = {
   }
 }
 
+# Bai 1
 puts params["supplier"]["bank_accounts"]["1"]["branch-name"]
+
+# Bai 2
+personal_hash = {
+  "supplier"=>{
+    "name"=>"Test lay params",
+    "tax_number"=>"0110254854",
+    "address"=>"Tokyo",
+    "supplier_type"=>"personal",
+    "bank_accounts"=>{
+      "0"=>{
+        "account"=>"847445",
+        "branch_name"=>"VCB"
+      },
+      "1591686336292"=>{
+        "account"=>"985784",
+        "branch_name"=>"ACB",
+        "_destroy"=>"false"
+      }
+    }
+  },
+  "type"=>"personal"
+}
+
+params["supplier"]["personal"] = personal_hash
+
+puts params["supplier"]
